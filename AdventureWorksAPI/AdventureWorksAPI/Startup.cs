41247c41790125cac11aaec95aeff27843961ba5
Carleton Cabarrus <carleton.cabarrus@onshoreoutsourcing.com>
2019-10-01 10:09:18 -0500

    installed api and json serializer to extract data from database

---
+++ AdventureWorksAPI/AdventureWorksAPI/Startup.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+using AdventureWorksAPI.DbModels;
+
+namespace AdventureWorksAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        readonly string PolicyName = "AdventureWorksAPI";
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddAuthentication(options =>
+            //{
+            //    options.DefaultAuthenticateScheme = OktaDefaults.ApiAuthenticationScheme;
+            //    options.DefaultChallengeScheme = OktaDefaults.ApiAuthenticationScheme;
+            //    options.DefaultSignInScheme = OktaDefaults.ApiAuthenticationScheme;
+            //})
+            //.AddOktaWebApi(new OktaWebApiOptions()
+            //{
+            //    OktaDomain = "https://dev-430760.okta.com"
+            //});
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1).AddJsonOptions(options =>
+            {
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+            });
+            services.AddDbContext<AdventureWorksContext>(options => options.UseSqlServer("Data Source=advworkscarl.database.windows.net;Initial Catalog=AdventureWorks;User ID=dev;Password=5611S@ddle;Connect Timeout=30;Encrypt=True;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False"));
+            services.AddCors(options =>
+            {
+                options.AddPolicy(PolicyName,
+                    builder =>
+                    {
+
+                        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader().AllowCredentials();
+                    });
+            });
+            services.AddSwaggerGen(c => c.SwaggerDoc("v1", new Info { Title = "AdventureWorksAPI", Version = "v1" }));
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseAuthentication();
+            app.UseCors(PolicyName);
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "AdventureWorksAPI v1");
+                c.RoutePrefix = string.Empty;
+            });
+        }
+    }
+}
